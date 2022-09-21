51f9d1098848d34a86151db6b4033722cc9e75c4
Felipe <pipecalgaro@gmail.com>
2022-09-21 15:07:37 -0300

    add cors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import { router as exercisesRouter } from './routes/exercises'
 import { router as dayRouter } from './routes/day'
 
 const app = express()
 
+app.use(cors({ origin: 'http://localhost:5173' }))
 app.use(express.json())
 
 app.get('/', (req, res) => {
