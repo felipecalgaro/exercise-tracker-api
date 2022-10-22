d95b8e787b0ed7724a0f2886857a8d5e211d9659
Felipe <pipecalgaro@gmail.com>
2022-10-22 13:50:46 -0300

    Develop API documentation

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import { router as exercisesRouter } from './routes/exercises'
 import { router as dayRouter } from './routes/day'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './swagger.json'
 
 const app = express()
 
@@ -10,5 +12,6 @@ app.use(express.json())
 
 app.use('/exercises', exercisesRouter)
 app.use('/days', dayRouter)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(process.env.PORT || 3333, () => console.log('server online'))
